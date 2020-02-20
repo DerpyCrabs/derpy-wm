729818c6dba387b084f0e14c651e7a0a1fb7f38e
DerpyCrabs <derpycrabs@gmail.com>
2020-02-21 02:06:43 +0700

    Split events in types and parse workspace events

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::process::Command;
 const GAP: usize = 5;
 
 #[derive(Debug, Clone, PartialEq)]
-enum EventType {
+enum WindowEventType {
     EnterNotify,
     CreateNotify,
     DestroyNotify,
@@ -13,9 +13,28 @@ enum EventType {
 }
 
 #[derive(Debug, Clone)]
-struct Event {
+enum WorkspaceEventType {
+    MoveWindow,
+    Focus,
+}
+
+#[derive(Debug, Clone)]
+struct WindowEvent {
     window_id: String,
-    event_type: EventType,
+    event_type: WindowEventType,
+}
+
+#[derive(Debug, Clone)]
+struct WorkspaceEvent {
+    workspace: usize,
+    event_type: WorkspaceEventType,
+}
+
+#[derive(Debug, Clone)]
+enum Event {
+    Window(WindowEvent),
+    Workspace(WorkspaceEvent),
+    Unknown,
 }
 
 fn parse_event(ev_str: Result<String>) -> Event {
@@ -24,16 +43,30 @@ fn parse_event(ev_str: Result<String>) -> Event {
         .split_whitespace()
         .map(ToOwned::to_owned)
         .collect();
-    let event_type = match ev_str_parts[0].as_str() {
-        "ENTER" => EventType::EnterNotify,
-        "CREATE" => EventType::CreateNotify,
-        "DESTROY" => EventType::DestroyNotify,
-        "MAP" => EventType::MapNotify,
-        _ => EventType::Unknown,
-    };
-    Event {
-        window_id: ev_str_parts[1].to_owned(),
-        event_type,
+    if ["ENTER", "CREATE", "DESTROY", "MAP"].contains(&ev_str_parts[0].as_str()) {
+        let event_type = match ev_str_parts[0].as_str() {
+            "ENTER" => WindowEventType::EnterNotify,
+            "CREATE" => WindowEventType::CreateNotify,
+            "DESTROY" => WindowEventType::DestroyNotify,
+            "MAP" => WindowEventType::MapNotify,
+            _ => unreachable!(),
+        };
+        Event::Window(WindowEvent {
+            window_id: ev_str_parts[1].to_owned(),
+            event_type,
+        })
+    } else if ["WS_FOCUS", "WS_MOVE"].contains(&ev_str_parts[0].as_str()) {
+        let event_type = match ev_str_parts[0].as_str() {
+            "WS_FOCUS" => WorkspaceEventType::Focus,
+            "WS_MOVE" => WorkspaceEventType::MoveWindow,
+            _ => unreachable!(),
+        };
+        Event::Workspace(WorkspaceEvent {
+            workspace: ev_str_parts[1].parse().expect("Invalid workspace event"),
+            event_type,
+        })
+    } else {
+        Event::Unknown
     }
 }
 
@@ -50,7 +83,7 @@ fn unmap_window(window_id: &str) {
     Command::new("mapw").arg("-u").arg(window_id).status().ok();
 }
 
-fn move_window(window_id: &str, x: usize, y: usize, w: usize, h: usize) {
+fn move_window(window_id: impl Into<String>, x: usize, y: usize, w: usize, h: usize) {
     Command::new("wtp")
         .args(
             vec![x, y, w, h]
@@ -58,7 +91,7 @@ fn move_window(window_id: &str, x: usize, y: usize, w: usize, h: usize) {
                 .map(|i| format!("{}", i))
                 .collect::<Vec<String>>(),
         )
-        .arg(window_id)
+        .arg(window_id.into())
         .status()
         .ok();
 }
@@ -77,17 +110,17 @@ fn tile_workspace(workspace: &Vec<String>) {
     match workspace.len() {
         0 => return,
         1 => {
-            move_window(workspace[0].as_str(), GAP, GAP, full_w, full_h);
+            move_window(&workspace[0], GAP, GAP, full_w, full_h);
         }
         2 => {
-            move_window(workspace[0].as_str(), GAP, GAP, half_w, full_h);
-            move_window(workspace[1].as_str(), half_w + GAP * 2, GAP, half_w, full_h);
+            move_window(&workspace[0], GAP, GAP, half_w, full_h);
+            move_window(&workspace[1], half_w + GAP * 2, GAP, half_w, full_h);
         }
         3 => {
-            move_window(workspace[0].as_str(), GAP, GAP, half_w, full_h);
-            move_window(workspace[1].as_str(), half_w + GAP * 2, GAP, half_w, half_h);
+            move_window(&workspace[0], GAP, GAP, half_w, full_h);
+            move_window(&workspace[1], half_w + GAP * 2, GAP, half_w, half_h);
             move_window(
-                workspace[2].as_str(),
+                &workspace[2],
                 half_w + GAP * 2,
                 half_h + GAP * 2,
                 half_w,
@@ -103,23 +136,11 @@ fn tile_workspace(workspace: &Vec<String>) {
             visible_windows
                 .iter()
                 .for_each(|wid| map_window(wid.as_str()));
-            move_window(visible_windows[0].as_str(), GAP, GAP, half_w, half_h);
+            move_window(&visible_windows[0], GAP, GAP, half_w, half_h);
+            move_window(&visible_windows[1], GAP, half_h + GAP * 2, half_w, half_h);
+            move_window(&visible_windows[2], half_w + GAP * 2, GAP, half_w, half_h);
             move_window(
-                visible_windows[1].as_str(),
-                GAP,
-                half_h + GAP * 2,
-                half_w,
-                half_h,
-            );
-            move_window(
-                visible_windows[2].as_str(),
-                half_w + GAP * 2,
-                GAP,
-                half_w,
-                half_h,
-            );
-            move_window(
-                visible_windows[3].as_str(),
+                &visible_windows[3],
                 half_w + GAP * 2,
                 half_h + GAP * 2,
                 half_w,
@@ -128,52 +149,57 @@ fn tile_workspace(workspace: &Vec<String>) {
         }
     }
 }
+
 fn main() {
     let mut workspaces: Vec<Vec<String>> = vec![Vec::new()];
     let mut focused_workspace = 0;
-    let mut last_event = Event {
-        window_id: "0x0".to_string(),
-        event_type: EventType::Unknown,
-    };
+    let mut last_event = Event::Unknown;
 
     for event in io::stdin().lock().lines().map(parse_event) {
+        let event_clone = event.clone();
         println!("{:#?}", event);
         println!("{:#?}", workspaces[0]);
-        match event.event_type {
-            EventType::MapNotify => {
-                if last_event.window_id == event.window_id
-                    && last_event.event_type == EventType::CreateNotify
-                {
-                    focus_window(event.window_id.as_str());
-                }
-                tile_workspace(&workspaces[focused_workspace]);
-            }
-            EventType::CreateNotify => {
-                workspaces[focused_workspace].push(event.window_id.clone());
-            }
-            EventType::DestroyNotify => {
-                let mut was_in_focused_workspace = false;
-                for (i, workspace) in workspaces.iter_mut().enumerate() {
-                    if let Some(_) = workspace
-                        .iter()
-                        .find(|wid| wid == &event.window_id.as_str())
-                    {
-                        if i == focused_workspace {
-                            was_in_focused_workspace = true;
+        match event {
+            Event::Window(event) => match event.event_type {
+                WindowEventType::MapNotify => {
+                    if let Event::Window(last_event) = last_event {
+                        if last_event.window_id == event.window_id
+                            && last_event.event_type == WindowEventType::CreateNotify
+                        {
+                            focus_window(event.window_id.as_str());
                         }
-                    }
-                    workspace.retain(|wid| wid.as_str() != event.window_id);
-                }
-
-                if was_in_focused_workspace {
-                    if let Some(window_id) = workspaces[focused_workspace].iter().last() {
-                        focus_window(window_id.as_str());
                     }
                     tile_workspace(&workspaces[focused_workspace]);
                 }
-            }
-            _ => (),
-        };
-        last_event = event;
+                WindowEventType::CreateNotify => {
+                    workspaces[focused_workspace].push(event.window_id);
+                }
+                WindowEventType::DestroyNotify => {
+                    let mut was_in_focused_workspace = false;
+                    for (i, workspace) in workspaces.iter_mut().enumerate() {
+                        if let Some(_) = workspace
+                            .iter()
+                            .find(|wid| wid == &event.window_id.as_str())
+                        {
+                            if i == focused_workspace {
+                                was_in_focused_workspace = true;
+                            }
+                        }
+                        workspace.retain(|wid| wid.as_str() != event.window_id);
+                    }
+
+                    if was_in_focused_workspace {
+                        if let Some(window_id) = workspaces[focused_workspace].iter().last() {
+                            focus_window(window_id.as_str());
+                        }
+                        tile_workspace(&workspaces[focused_workspace]);
+                    }
+                }
+                _ => (),
+            },
+            Event::Workspace(event) => {}
+            Event::Unknown => {}
+        }
+        last_event = event_clone;
     }
 }
--- todo.md
+++ todo.md
@@ -1,6 +1,8 @@
 - workspaces
 - window movement
 - retile windows on window move
+- borders
 - fullscreen toggle
+- functional tests
 - scratchpad
 - support two monitors
